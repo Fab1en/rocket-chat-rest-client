d3c1a5a5b599f481c923358438904dd387d720ad
Fabien Quatravaux <fabien.quatravaux@happy-dev.fr>
2016-12-27 16:45:33 +0100

    [refactor] Move User methods to its own class

--- RocketChatClient.php
+++ RocketChatClient.php
@@ -6,13 +6,10 @@ use Httpful\Request;
 
 class Client{
 
-	private $rc_instance;
-	private $api_root = '/api/v1/';
-	private $api;
+	public $api;
 
-	function __construct($url){
-		$this->rc_instance = $url;
-		$this->api = $this->rc_instance . $this->api_root;
+	function __construct(){
+		$this->api = ROCKET_CHAT_INSTANCE . REST_API_ROOT;
 
 		// set template request to send and expect JSON
 		$tmp = Request::init()
@@ -27,24 +24,6 @@ class Client{
 	public function version() {
 		$response = \Httpful\Request::get( $this->api . 'info' )->send();
 		return $response->body->info->version;
-	}
-
-	/**
-	* Authenticate with the REST API.
-	*/
-	public function login( $username, $password ) {
-		$response = Request::post( $this->api . 'login' )
-			->body(array( 'user' => $username, 'password' => $password ))
-			->send();
-
-		if( $response->body->status == 'success' ){
-			// save auth token for future requests
-			$tmp = Request::init()
-				->addHeader('X-Auth-Token', $response->body->data->authToken)
-				->addHeader('X-User-Id', $response->body->data->userId);
-			Request::ini( $tmp );
-			return true;
-		}
 	}
 
 	/**
@@ -74,43 +53,6 @@ class Client{
 
 		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
 			return $response->body->users;
-		} else {
-			echo( $response->body->error . "\n" );
-			return false;
-		}
-	}
-
-	/**
-	* Create a new user.
-	*/
-	public function create_user( $nickname, $email, $username, $password, $customFields = array() ) {
-		$response = Request::post( $this->api . 'users.create' )
-			->body(array( 
-				'name' => $nickname,
-				'email' => $email,
-				'username' => $username,
-				'password' => $password,
-			))
-			->send();
-
-		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
-			return $response->body->user;
-		} else {
-			echo( $response->body->error . "\n" );
-			return false;
-		}
-	}
-
-	/**
-	* Deletes an existing user.
-	*/
-	public function delete_user( $userId ) {
-		$response = Request::post( $this->api . 'users.delete' )
-			->body(array('userId' => $userId))
-			->send();
-
-		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
-			return true;
 		} else {
 			echo( $response->body->error . "\n" );
 			return false;
---
+++ RocketChatUser.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace RocketChat;
+require_once 'lib/httpful.phar';
+use Httpful\Request;
+use RocketChat\Client;
+
+class User extends Client {
+	public $username;
+	private $password;
+	public $id;
+	public $nickname;
+	public $email;
+
+	public function __construct($username, $password, $fields = array()){
+		parent::__construct();
+		$this->username = $username;
+		$this->password = $password;
+		if( isset($fields['nickname']) ) {
+			$this->nickname = $fields['nickname'];
+		}
+		if( isset($fields['email']) ) {
+			$this->email = $fields['email'];
+		}
+	}
+
+	/**
+	* Authenticate with the REST API.
+	*/
+	public function login($save_auth = true) {
+		$response = Request::post( $this->api . 'login' )
+			->body(array( 'user' => $this->username, 'password' => $this->password ))
+			->send();
+
+		if( $response->code == 200 && isset($response->body->status) && $response->body->status == 'success' ) {
+			if( $save_auth) {
+				// save auth token for future requests
+				$tmp = Request::init()
+					->addHeader('X-Auth-Token', $response->body->data->authToken)
+					->addHeader('X-User-Id', $response->body->data->userId);
+				Request::ini( $tmp );
+			}
+			$this->id = $response->body->data->userId;
+			return true;
+		} else {
+			echo( $response->body->message . "\n" );
+			return false;
+		}
+	}
+
+	/**
+	* Gets a user’s information, limited to the caller’s permissions.
+	*/
+	public function info() {
+		$response = Request::get( $this->api . 'users.info?userId=' . $this->id )->send();
+
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			$this->id = $response->body->user->_id;
+			$this->nickname = $response->body->user->name;
+			$this->email = $response->body->user->emails[0]->address;
+			return $response->body;
+		} else {
+			echo( $response->body->error . "\n" );
+			return false;
+		}
+	}
+
+	/**
+	* Create a new user.
+	*/
+	public function create() {
+		$response = Request::post( $this->api . 'users.create' )
+			->body(array( 
+				'name' => $this->nickname,
+				'email' => $this->email,
+				'username' => $this->username,
+				'password' => $this->password,
+			))
+			->send();
+
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			$this->id = $response->body->user->_id;
+			return $response->body->user;
+		} else {
+			echo( $response->body->error . "\n" );
+			return false;
+		}
+	}
+
+	/**
+	* Deletes an existing user.
+	*/
+	public function delete() {
+
+		// get user ID if needed
+		if( !isset($this->id) ){
+			$this->me();
+		}
+		$response = Request::post( $this->api . 'users.delete' )
+			->body(array('userId' => $this->id))
+			->send();
+
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			return true;
+		} else {
+			echo( $response->body->error . "\n" );
+			return false;
+		}
+	}
+}
