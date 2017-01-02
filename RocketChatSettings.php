043b5d9de03ad685a0a453db989a1225f6e53d7e
Fabien Quatravaux <fabien.quatravaux@happy-dev.fr>
2017-01-02 09:27:14 +0100

    [fix] Various fixes for settings usage
     * Display a message for JSON decoding errors
     * Make methods chainable
     * Implement update method
     * Enable object comparison
     * Do not escape slashes

--- RocketChatSettings.php
+++ RocketChatSettings.php
@@ -35,6 +35,21 @@ class Settings extends Client {
 	}
 
 	/**
+	* Updates a setting.
+	*/
+	public function update( $id, $value ){
+		$response = Request::post( $this->api . 'settings/' . $id )
+			->body(array("value" => $value))
+			->send();
+
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			return true;
+		} else {
+			echo( $response->body->error . "\n" );
+		}
+	}
+
+	/**
 	* Load the local setting file content and check if its value is the same
 	* as online. Print the difference if it finds one.
 	*/
@@ -42,18 +57,34 @@ class Settings extends Client {
 		$f = fopen($this->file, 'r');
 		$settings = json_decode( fread($f, filesize($this->file)) );
 		fclose($f);
+
+		if( $settings === null ) {
+			echo "Erreur de décodage json pour le fichier {$this->file}\n";
+			return;
+		}
+
 		foreach($settings as $id => $value){
 			$check_val = $this->get($id);
-			if( $value !== $check_val ) {
+			
+			if(is_object($value) && is_object($check_val) && $value == $check_val) {
+				// object comparison
+				continue;
+			} else if( $value !== $check_val ) {
 				if($check_val === true) $check_val = 'true';
 				if($check_val === false) $check_val = 'false';
 				if($check_val === "") $check_val = '""';
+				if(is_object($check_val)) $check_val = print_r($check_val, true);
 				if($value === true) $value = 'true';
 				if($value === false) $value = 'false';
 				if($value === "") $value = '""';
+				if(is_object($value)) $value = print_r($value, true);
+
 				echo "$id : $check_val instead of $value\n";
 			}
 		}
+
+		// make method chainable
+		return $this;
 	}
 
 	/**
@@ -67,8 +98,11 @@ class Settings extends Client {
 			$settings->{$id} = $this->get($id);
 		}
 		$f = fopen($this->file, 'w');
-		fwrite($f, json_encode($settings, JSON_PRETTY_PRINT));
+		fwrite($f, json_encode($settings, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
 		fclose($f);
+
+		// make method chainable
+		return $this;
 	}
 
 	/**
@@ -81,6 +115,9 @@ class Settings extends Client {
 		foreach($settings as $id => $value){
 			$this->update($id, $value);
 		}
+
+		// make method chainable
+		return $this;
 	}
 }
 
